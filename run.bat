32c4c5cdf4488610637db9b3c8ea63f47b3b81af
wustzhh <2212366824@qq.com>
2026-08-04 14:51:02 +0800

    run.bat: 恢复为直接启动，编译由agent负责

--- run.bat
+++ run.bat
@@ -1,12 +1,4 @@
 @echo off
 cd /d "%~dp0"
 taskkill /F /IM gomusic.exe >nul 2>&1
-echo Building...
-D:\app-dev\flutter\bin\flutter.bat build windows
-if %errorlevel% neq 0 (
-    echo Build failed!
-    pause
-    exit /b %errorlevel%
-)
-echo Starting...
 start "" "build\windows\x64\runner\Release\gomusic.exe"
