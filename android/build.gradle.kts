51e0504d087f5604ffe335ca4b5b863855d2de54
wustzhh <2212366824@qq.com>
2026-08-07 12:08:29 +0800

    build: 安卓构建配置(国内镜像/jdk路径/kotlin非增量)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 allprojects {
     repositories {
-        google()
-        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
     }
 }
 
